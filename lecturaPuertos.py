2b62b50dcdd91e664b8b4669f54c8786420af1a0
Cesar-580 <caospinamu@unal.edu.co>
2022-03-10 21:39:56 -0500

    Se lee la trama, se separa cada uno de los valores estos se convierten a decimal, luego binario y por último se completa para que ocupe exactamente un(1) Byte

--- lecturaPuertos.py
+++ lecturaPuertos.py
@@ -16,49 +16,80 @@ ser = serial.Serial('COM9',115200)
 
 x = 0
 
-print("Trama uno")
-while x < 21:
-    datosLeidos = ser.read()
-    datos = datosLeidos
-    #print(datos)
-    #print(type(datos))
-    x+= 1
+#print("Trama uno")
 
-print("Trama dos")
-datosLeidosDos = ser.read(20)
-datosDos = datosLeidosDos
-print(datosDos)
-print(datosDos[1])
-print("TIPOSSSSSSSSSSSSSSSSSSSSSSSS")
-print(type(datosDos))
-print(type(datosDos[1]))
+# Lista almacenadora de los datos
+            # cadenaHexadecimal = []
 
+            # print("Trama uno")
+            # while x < 21:
+            #     datosLeidos = ser.read()
+            #     datos = datosLeidos
+            #     #print(x)
+            #     #print(datos)
+            #     print(f'{datos:0>8b}', end=' ')
+            #     #print(type(datos))
+            #     x+= 1
+
+            # print(cadenaHexadecimal)
+
+datosLeidos = ser.read(21)
+# for i in range(0,21):
+#     print(datosLeidos[i])
+
+def binar(dec):
+    bina = ""
+    while dec // 2 != 0:
+        bina = str(dec % 2) + bina
+        dec = dec // 2
+    return str(dec) + bina
+
+def completarCeros(nBinario):
+    nBinario = nBinario.zfill(8)
+    return nBinario
+
+tramaDecimal = [datosLeidos[i] for i in range(0,21)]
+# print(tramaDecimal)
+tramaBinaria = list(map(binar, tramaDecimal))
+print(tramaBinaria)
+tramaBinariaCompleto = list(map(completarCeros, tramaBinaria))
+print(tramaBinariaCompleto)
+
+
+
+#print(len(cadenaHexadecimal))
+#print(int((cadenaHexadecimal[1]),2))
+
+#datosLeidosDos = ser.read(20)
+#datosDos = datosLeidosDos
+# datosDos = (datosDos[1:-1])
+
+# for i in range(0,19):
+#     print(datosDos[i])
+
+
+#print(datosDos)
+#print(datosDos[1])
+#print("TIPOSSSSSSSSSSSSSSSSSSSSSSSS")
+#print(type(datosDos))
+#print(datosDos[5])
+
+a = bin(2)
+b = bin(2)
+
+#print(type(a))
+
+# r = a[2:] + b[2:]
+# print(type(r))
+# r = r.zfill(8)
+# print(type(int(r,2)))
+# print(bin(int(r,2)))
+# print(int(r,2))
 
 
 
 ser.close()             # close port
 
 
-
-# def bin(dec):
-#     bina = ""
-#     while dec // 2 != 0:
-#         bina = str(dec % 2) + bina
-#         dec = dec // 2
-#     return str(dec) + bina
-
-
-
-# print(int(bin(9),2))
-zeros = 00000000
-n = int(bin(2),2)
-print(n)
-print(type(n))
-a = 0b1101
-b = 0b1011
-r = bin(a | b)
-
-print(type(a))
-print(type(b))
 
 
